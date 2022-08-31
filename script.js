632e9c7975ebf0cf677629501752b58d42886413
akash@22 <100702414+neekhara22@users.noreply.github.com>
2022-09-01 01:08:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const wrapper = document.querySelector(".wrapper"),
+qrInput= wrapper.querySelector(".form input"),
+generateBtn= wrapper.querySelector(".form button"),
+qrImg= wrapper.querySelector(".qr-code img");
+let preValue;
+
+generateBtn.addEventListener("click",()=>{
+    let qrValue = qrInput.value.trim();
+    if(!qrValue || preValue === qrValue) return;  //if the input is empty then return from here
+    preValue = qrValue;
+    generateBtn.innerText = "Generating QR Code..."
+    //getting a QR code of user entered value using the qrserver
+    //api and passing the api returned img src to qrImg
+    qrImg.src =`https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+    qrImg.addEventListener("load",()=>{ //once QR code img loaded
+    wrapper.classList.add("active");
+    generateBtn.innerText = "Generate QR Code"
+    });
+});
+
+qrInput.addEventListener("keyup",()=>{
+    if(!qrInput.value){
+        wrapper.classList.remove("active");
+        preValue = "";
+    }
+});
